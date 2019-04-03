889da73320c5eef38c22230dcf9f9a18fc91c006
root <root@roi-test.upress.io>
2019-04-03 02:42:25 -0400

    finished:   ex1.py

--- beginning/ex1.py
+++ beginning/ex1.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3.6
+#############################
+#
+#	write python script that will print out your name
+#
+#############################
+
+
 first='Roi'
 last='Tiefenbrunn'
 print (first+' '+last)
